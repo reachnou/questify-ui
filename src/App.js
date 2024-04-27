f42532de81ac4000be0e6e894497c27365ab9a9d
noureach <rnou@miu.edu>
2024-04-27 03:39:57 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import MyRoutes from "./utils/MyRoutes";
 
 function App() {
-  console.log("Here");
   return (
     <Router>
       <MyRoutes />
